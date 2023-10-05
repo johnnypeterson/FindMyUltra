d76f7e39325b019413604070aaab0b8135ad1ae6
Johnny <johnny.d.peterson@gmail.com>
2023-10-05 14:20:31 -0700

    Initial Commit

---
+++ FindMyUltra/FindMyUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyUltraApp.swift
+//  FindMyUltra
+//
+//  Created by Johnny Peterson on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FindMyUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
